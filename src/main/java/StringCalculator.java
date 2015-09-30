a41306b59edd595e287c7a17c843ec4910eb6ce1
Luis Burgos <luisarturo.burgos@gmail.com>
2015-09-30 14:39:29 -0500

    First commit

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,13 @@
+/**
+ * Created by luisburgos on 30/09/15.
+ */
+public class StringCalculator {
+
+    public int add(String numbers){
+        int sumNumbers = 0;
+
+
+        return sumNumbers;
+    }
+
+}
